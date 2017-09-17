6da9616b1338b1579b7b683eef2db520e3261a92
Aviel Aldama <aviel.aldama@gmail.com>
2017-09-17 15:31:33 -0500

    Add project files.

---
+++ ECommerce/Models/Department.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ECommerce.Models
+{
+    public class Department
+    {
+        [Key]
+        public int DepartmentId { get; set; }
+
+        [Required(ErrorMessage = "The Field {0} is required")]
+        [MaxLength(50, ErrorMessage ="The Field {0} must be maximum {1} characters length")]
+        [Display(Name = "Department")]
+        public string Name { get; set; }
+
+        public virtual ICollection<City> Cities { get; set; }
+
+
+    }
+}
