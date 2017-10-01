2d8651a96b3c2c9ec8572ec0c1cf1d10633c81f8
Aviel Aldama <aviel.aldama@gmail.com>
2017-09-30 21:59:39 -0500

    Taxes and Product CRUD

---
+++ ECommerce/Controllers/ProductsController.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ECommerce.Models;
+using ECommerce.Classes;
+
+namespace ECommerce.Controllers
+{
+    [Authorize(Roles = "User")]
+    public class ProductsController : Controller
+    {
+        private ECommerceContext db = new ECommerceContext();
+
+        // GET: Products
+        public ActionResult Index()
+        {
+            var user = db.Users
+                .Where(u => u.UserName == User.Identity.Name)
+                .FirstOrDefault();
+
+            var products = db.Products
+                .Include(p => p.Category)
+                .Include(p => p.Tax)
+                .Where(p => p.CompanyId == user.CompanyId);
+
+            return View(products.ToList());
+        }
+
+        // GET: Products/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // GET: Products/Create
+        public ActionResult Create()
+        {
+            var user = db.Users
+                .Where(u => u.UserName == User.Identity.Name)
+                .FirstOrDefault();
+
+            ViewBag.CategoryId = new SelectList(
+                CombosHelper.GetCategories(user.CompanyId), 
+                "CategoryId", 
+                "Description");
+
+            ViewBag.TaxId = new SelectList(
+                CombosHelper.GetTaxes(user.CompanyId), 
+                "TaxId", 
+                "Description");
+
+            var product = new Product {
+                CompanyId = user.CompanyId
+            };
+
+            return View(product);
+        }
+
+        // POST: Products/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Product product)
+        {
+            var user = db.Users
+                .Where(u => u.UserName == User.Identity.Name)
+                .FirstOrDefault();
+
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+
+                if (user.PhotoFile!=null)
+                {
+                    var folder = "~/Content/Products";
+
+                    var file = string.Format("{0}.jpg", 
+                        product.ProductId);
+
+                    var response = FilesHelper.UploadPhoto(
+                        product.ImageFile, folder, file);
+
+                    if (response)
+                    {
+                        var pic = string.Format("{0}/{1}", folder, file);
+                        product.Image = pic;
+                        db.Entry(product).State = EntityState.Modified;
+                        db.SaveChanges();
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryId = new SelectList(
+                CombosHelper.GetCategories(user.CompanyId), 
+                "CategoryId", 
+                "Description", 
+                product.CategoryId);
+
+            ViewBag.TaxId = new SelectList(
+                CombosHelper.GetTaxes(user.CompanyId), 
+                "TaxId", 
+                "Description", 
+                product.TaxId);
+
+            return View(product);
+        }
+
+        // GET: Products/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Product product = db.Products.Find(id);
+
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+
+            ViewBag.CategoryId = new SelectList(
+                CombosHelper.GetCategories(product.CompanyId), 
+                "CategoryId", 
+                "Description", 
+                product.CategoryId);
+
+            ViewBag.TaxId = new SelectList(
+                CombosHelper.GetTaxes(product.CompanyId), 
+                "TaxId", 
+                "Description", 
+                product.TaxId);
+
+            return View(product);
+        }
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Product product)
+        {
+
+            if (ModelState.IsValid)
+            {
+                if (product.ImageFile!=null)
+                {
+                    var folder = "~/Content/Products";
+
+                    var file = string.Format("{0}.jpg",
+                        product.ProductId);
+
+                    var response = FilesHelper.UploadPhoto(
+                        product.ImageFile, 
+                        folder, file);
+
+                    if (response)
+                    {
+                        var pic = string.Format($"{folder}/{file}");
+                        product.Image = pic;
+                    }
+                }
+
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryId = new SelectList(
+                CombosHelper.GetCategories(product.CompanyId), 
+                "CategoryId", 
+                "Description", 
+                product.CategoryId);
+
+            ViewBag.TaxId = new SelectList(
+                CombosHelper.GetTaxes(product.CompanyId), 
+                "TaxId", 
+                "Description", 
+                product.TaxId);
+
+            return View(product);
+        }
+
+        // GET: Products/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Product product = db.Products.Find(id);
+            db.Products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ECommerce/Models/Tax.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ECommerce.Models
+{
+    public class Tax
+    {
+        [Key]
+        public int TaxId { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [MaxLength(50, ErrorMessage ="The field {0} must be maximum {1} characters length")]
+        [Index("Tax_CompanyId_Description_Index", 2, IsUnique =true)]
+        [Display(Name = "Tax")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage ="The field {0} is required")]
+        [DisplayFormat(DataFormatString = "{0:P2}", ApplyFormatInEditMode =false)]
+        [Range(0, 1, ErrorMessage ="You must select a {0} between {1} and {2}")]
+        public double Rate { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [Range(1, double.MaxValue, ErrorMessage = "You must select a {0}")]
+        [Index("Tax_CompanyId_Description_Index", 1, IsUnique =true)]
+        [Display(Name ="Company")]
+        public int CompanyId { get; set; }
+
+        public virtual Company Company { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
+
+    }
+}
